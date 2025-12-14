d30712c44e1587aa82f66cdb908d931fafda8947
daniel-got <xiaoxaniel@gmail.com>
2025-12-14 12:41:11 +0700

    update post destinations

--- backend/flake.nix
+++ backend/flake.nix
@@ -23,6 +23,7 @@
           packages = with pkgs; [
             python314
             python313Packages.pip
+            postgresql
           ];
           shellHook = ''
             echo "Hello from devShell!"
--- backend/views/destinations/destinations_view.py
+++ backend/views/destinations/destinations_view.py
@@ -6,28 +6,22 @@ from pyramid.view import view_config
 from pydantic import BaseModel, ValidationError
 from pyramid.response import Response
 from models.destination_model import Destination
+from . import serialization_data
+from helpers.jwt_validate_helper import jwt_validate
 
 
-class FromRequest(BaseModel):
+class DestinationRequest(BaseModel):
     country: Optional[str] = None
     name: Optional[str] = None
-
-
-def serialization_data(destination):
-    return {
-        "id": str(destination.id),
-        "name": destination.name,
-        "description": destination.description,
-        "photoUrl": destination.photo_url,
-        "country": destination.country,
-    }
+    description: Optional[str] = None
+    photo_url: Optional[str] = None
 
 
 @view_config(route_name="destinations", request_method="GET", renderer="json")
 def destinations(request):
     # request validation
     try:
-        req_data = FromRequest(**request.params.mixed())
+        req_data = DestinationRequest(**request.params.mixed())
     except ValidationError as err:
         return Response(json_body={"error": str(err.errors())}, status=400)
 
@@ -67,4 +61,41 @@ def destination_detail(request):
             print(e)
             return Response(
                 json_body={"error": "Invalid ID or server error"}, status=400
+            )
+
+
+@view_config(route_name="destinations", request_method="POST", renderer="json")
+@jwt_validate
+def create_destinations(request):
+    if request.jwt_claims["role"] != "agent":
+        return Response(
+            json_body={"error": "Forbidden : Only agent can access"}, status=403
+        )
+
+    try:
+        req_data = DestinationRequest(**request.json_body)
+    except ValidationError as err:
+        return Response(json_body={"error": str(err.errors())}, status=400)
+
+    with Session() as session:
+        new_destination = Destination(
+            name = req_data.name,
+            description= req_data.description,
+            photo_url= req_data.photo_url,
+            country = req_data.country,
+        )
+
+        try:
+            session.add(new_destination)
+            session.commit()
+            session.refresh(new_destination)
+            return serialization_data(new_destination)
+        except IntegrityError as err:
+            session.rollback()
+            return Response(json_body={"error": str(err.orig)}, status=409)
+        except Exception as e:
+            session.rollback()
+            print(f"CRITICAL ERROR: {e}")
+            return Response(
+                json_body={"error": f"Internal Server Error: {str(e)}"}, status=500
             )
